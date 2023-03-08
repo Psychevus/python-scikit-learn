007238a8f055fe7c94565275794906ccd3368b43
Psychevus <Mojtaba.zaferanloo@gmail.com>
2023-03-08 18:23:20 +0330

    optimized
    Added a main() function to encapsulate the main logic of the script.
    Used more descriptive function and variable names to improve readability.
    Moved the import statements to the top of the script to conform to PEP 8.
    Used 4 spaces for indentation, which is the recommended style in Python.
    Removed unnecessary comments and whitespace to improve readability.
    Added a docstring to the main() function to improve documentation.

--- scikit-learn-sample.py
+++ scikit-learn-sample.py
@@ -2,20 +2,26 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-# Generate random data
-np.random.seed(42)
-X = np.random.rand(100, 1)
-y = 2 + 3 * X + np.random.randn(100, 1)
+def generate_data(n_samples):
+    """Generate random data for linear regression."""
+    np.random.seed(42)
+    X = np.random.rand(n_samples, 1)
+    y = 2 + 3 * X + np.random.randn(n_samples, 1)
+    return X, y
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+def train_and_evaluate_model(X_train, y_train, X_test, y_test):
+    """Train and evaluate a linear regression model."""
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    r_squared = model.score(X_test, y_test)
+    return r_squared
 
-# Train the model
-model = LinearRegression()
-model.fit(X_train, y_train)
+def main():
+    """Main function for running the script."""
+    X, y = generate_data(n_samples=100)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    r_squared = train_and_evaluate_model(X_train, y_train, X_test, y_test)
+    print(f"R-squared score: {r_squared:.4f}")
 
-# Evaluate the model
-score = model.score(X_test, y_test)
-print("R-squared score:", score)
-
-# This code should output the R-squared score for our model on the testing data.
+if __name__ == '__main__':
+    main()
