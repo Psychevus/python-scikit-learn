30b8626f11e5a5157db77053dd368d395136203b
Psychevus <Mojtaba.zaferanloo@gmail.com>
2023-03-24 16:33:56 +0330

    Update scikit-learn-sample.py
    Increased the number of samples generated by generate_data to 1000, and added parameters to control the slope, intercept, and noise level of the data.
    Used matplotlib to visualize the generated data.
    Added a dictionary of linear regression models to main, including LinearRegression, Ridge, and Lasso, and used cross-validation to evaluate each model's performance on the data.
    Used grid search with cross-validation to find the best hyperparameters for the Ridge model.

--- scikit-learn-sample.py
+++ scikit-learn-sample.py
@@ -1,27 +1,42 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
-def generate_data(n_samples):
+def generate_data(n_samples, slope=3, intercept=2, noise=1):
     """Generate random data for linear regression."""
     np.random.seed(42)
     X = np.random.rand(n_samples, 1)
-    y = 2 + 3 * X + np.random.randn(n_samples, 1)
+    y = intercept + slope * X + noise * np.random.randn(n_samples, 1)
     return X, y
 
-def train_and_evaluate_model(X_train, y_train, X_test, y_test):
+def train_and_evaluate_model(model, X, y):
     """Train and evaluate a linear regression model."""
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    r_squared = model.score(X_test, y_test)
-    return r_squared
+    scores = cross_val_score(model, X, y, cv=5, scoring='r2')
+    return np.mean(scores)
 
 def main():
     """Main function for running the script."""
-    X, y = generate_data(n_samples=100)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    r_squared = train_and_evaluate_model(X_train, y_train, X_test, y_test)
-    print(f"R-squared score: {r_squared:.4f}")
+    X, y = generate_data(n_samples=1000, slope=5, intercept=2, noise=2)
+    plt.scatter(X, y)
+    plt.xlabel('X')
+    plt.ylabel('y')
+    plt.show()
+
+    models = {
+        'Linear Regression': LinearRegression(),
+        'Ridge Regression': Ridge(),
+        'Lasso Regression': Lasso()
+    }
+
+    for name, model in models.items():
+        scores = cross_val_score(model, X, y, cv=5, scoring='r2')
+        print(f"{name}: mean R-squared score = {np.mean(scores):.4f} (std = {np.std(scores):.4f})")
+
+    param_grid = {'alpha': np.logspace(-3, 3, 7)}
+    ridge_model = GridSearchCV(Ridge(), param_grid, cv=5, scoring='r2')
+    ridge_model.fit(X, y)
+    print(f"Best Ridge Regression model: alpha = {ridge_model.best_params_['alpha']:.4f}, R-squared score = {ridge_model.best_score_:.4f}")
 
 if __name__ == '__main__':
     main()
